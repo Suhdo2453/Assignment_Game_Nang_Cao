762221971b5235b84517a3105e147a4ebf832bd6
Suhdo <dungvipnr@gmail.com>
2023-04-07 13:14:18 +0700

    finish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int maxScore = 10;
     [SerializeField] private Slider coinBar;
-    private int _score = 0;
+    private int _score;
 
     private void Start()
     {
@@ -24,5 +26,15 @@ public class GameManager : MonoBehaviour
 
         _score++;
         coinBar.value = _score;
+        if (_score == maxScore)
+        {
+            StartCoroutine("LoadScene");
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
     }
 }
